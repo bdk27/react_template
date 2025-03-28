0ffd696cae513ffd02dfa92f6b650690d2126c10
YCheng <briansam195@gmail.com>
2025-03-28 14:31:38 +0800

    新增 Test 組件並在 App 中使用，更新 Vite 配置以支援路徑別名

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Test from "@/components/Test.jsx";
+
 function App() {
   return (
     <>
       <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <Test />
     </>
   );
 }
